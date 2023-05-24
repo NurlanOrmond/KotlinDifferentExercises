f126994852e533c03e43d460d864222a73c7746c
Nurlan Ormond <sunshineforester@gmail.com>
2023-05-24 17:25:03 +0300

    Task 26, 28

---
+++ src/main/kotlin/Python_HWs/Task_28.kt
@@ -0,0 +1,17 @@
+package Python_HWs
+// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с
+// помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+//    A = 2; B = 3 -> 8
+
+private fun power(a: Int, b: Int): Int {
+    return if (b == 0) 1
+    else
+        a * power(a, b - 1)
+}
+
+fun main() {
+    println(power(3, 5))
+}
+
+
